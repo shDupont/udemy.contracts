9dcd2bc45772599596b61f822f9de6af49bdbe9d
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-23 18:41:27 -0300

    Initial commit

---
+++ src/entities/ContractService.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class ProcessingService {
+
+    private Installment installment;
+
+    public void processContract(Contract contract){
+        long time = contract.getDate().getTime();
+
+
+    }
+}
